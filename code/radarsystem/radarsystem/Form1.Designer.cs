dd32f7a6c4c087b6c4f9c2db4b442f5f0cba0dc9
lyan <liyiloving520@sina.com>
2015-09-03 12:30:24 +0800

    addcode

--- code/radarsystem/radarsystem/Form1.Designer.cs
+++ code/radarsystem/radarsystem/Form1.Designer.cs
@@ -35,9 +35,14 @@
             this.tbp_SceneSet = new System.Windows.Forms.TabPage();
             this.tbp_SetModule = new System.Windows.Forms.TabPage();
             this.tbp_CharacterAnalysis = new System.Windows.Forms.TabPage();
+            this.cob_setModules = new System.Windows.Forms.ComboBox();
+            this.clb_setMod = new System.Windows.Forms.CheckedListBox();
+            this.label_addNoiseType = new System.Windows.Forms.Label();
+            this.btn_Finish = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.tabControl1.SuspendLayout();
+            this.tbp_SetModule.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -80,6 +85,10 @@
             // 
             // tbp_SetModule
             // 
+            this.tbp_SetModule.Controls.Add(this.btn_Finish);
+            this.tbp_SetModule.Controls.Add(this.label_addNoiseType);
+            this.tbp_SetModule.Controls.Add(this.clb_setMod);
+            this.tbp_SetModule.Controls.Add(this.cob_setModules);
             this.tbp_SetModule.Location = new System.Drawing.Point(4, 22);
             this.tbp_SetModule.Name = "tbp_SetModule";
             this.tbp_SetModule.Padding = new System.Windows.Forms.Padding(3);
@@ -97,6 +106,59 @@
             this.tbp_CharacterAnalysis.Text = "特性分析";
             this.tbp_CharacterAnalysis.UseVisualStyleBackColor = true;
             // 
+            // cob_setModules
+            // 
+            this.cob_setModules.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cob_setModules.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cob_setModules.FormattingEnabled = true;
+            this.cob_setModules.Items.AddRange(new object[] {
+            "多普勒雷达",
+            "多基地雷达",
+            "超视距雷达",
+            "声呐雷达"});
+            this.cob_setModules.Location = new System.Drawing.Point(398, 6);
+            this.cob_setModules.Name = "cob_setModules";
+            this.cob_setModules.Size = new System.Drawing.Size(121, 22);
+            this.cob_setModules.TabIndex = 0;
+            this.cob_setModules.SelectedIndexChanged += new System.EventHandler(this.cob_setModules_SelectedIndexChanged);
+            // 
+            // clb_setMod
+            // 
+            this.clb_setMod.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.clb_setMod.CheckOnClick = true;
+            this.clb_setMod.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.clb_setMod.FormattingEnabled = true;
+            this.clb_setMod.HorizontalScrollbar = true;
+            this.clb_setMod.Items.AddRange(new object[] {
+            "高斯白噪声",
+            "泊松噪声",
+            "平均噪声"});
+            this.clb_setMod.Location = new System.Drawing.Point(412, 72);
+            this.clb_setMod.Name = "clb_setMod";
+            this.clb_setMod.Size = new System.Drawing.Size(120, 54);
+            this.clb_setMod.TabIndex = 1;
+            this.clb_setMod.SelectedIndexChanged += new System.EventHandler(this.clb_setMod_SelectedIndexChanged);
+            // 
+            // label_addNoiseType
+            // 
+            this.label_addNoiseType.AutoSize = true;
+            this.label_addNoiseType.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_addNoiseType.Location = new System.Drawing.Point(395, 44);
+            this.label_addNoiseType.Name = "label_addNoiseType";
+            this.label_addNoiseType.Size = new System.Drawing.Size(98, 14);
+            this.label_addNoiseType.TabIndex = 2;
+            this.label_addNoiseType.Text = "添加噪声种类:";
+            // 
+            // btn_Finish
+            // 
+            this.btn_Finish.Location = new System.Drawing.Point(398, 142);
+            this.btn_Finish.Name = "btn_Finish";
+            this.btn_Finish.Size = new System.Drawing.Size(75, 23);
+            this.btn_Finish.TabIndex = 3;
+            this.btn_Finish.Text = "选择完毕";
+            this.btn_Finish.UseVisualStyleBackColor = true;
+            this.btn_Finish.Click += new System.EventHandler(this.btn_Finish_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -110,6 +172,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.tabControl1.ResumeLayout(false);
+            this.tbp_SetModule.ResumeLayout(false);
+            this.tbp_SetModule.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -122,6 +186,11 @@
         private System.Windows.Forms.TabPage tbp_SceneSet;
         private System.Windows.Forms.TabPage tbp_SetModule;
         private System.Windows.Forms.TabPage tbp_CharacterAnalysis;
+        private System.Windows.Forms.CheckedListBox clb_setMod;
+        private System.Windows.Forms.ComboBox cob_setModules;
+        private System.Windows.Forms.Label label_addNoiseType;
+        private System.Windows.Forms.Button btn_Finish;
+        private string strCollected;
     }
 }
 
--- code/radarsystem/radarsystem/Form1.cs
+++ code/radarsystem/radarsystem/Form1.cs
@@ -15,5 +15,66 @@ namespace radarsystem
         {
             InitializeComponent();
         }
+
+        private void cob_setModules_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(cob_setModules.SelectedIndex)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    MessageBox.Show("请选择雷达类型！");
+                    break;
+            }
+        }
+
+        private void clb_setMod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i;
+            strCollected = string.Empty;
+            for (i = 0; i < clb_setMod.Items.Count; i++)
+            {
+                if (clb_setMod.GetItemChecked(i))
+                {
+                    if (strCollected == string.Empty)
+                    {
+                        strCollected = clb_setMod.GetItemText(clb_setMod.Items[i]);
+                    }
+                    else
+                    {
+                        strCollected = strCollected + "，" + clb_setMod.GetItemText(clb_setMod.Items[i]);
+                    }
+                }
+            }
+            //MessageBox.Show("您选择添加了" + strCollected, "提示");
+            //if (clb_setMod.GetItemChecked(0))
+            //{
+            //    MessageBox.Show("添加了泊松噪声！");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("未添加泊松噪声！");
+            //}
+
+            //if (clb_setMod.GetItemChecked(1))
+            //{
+            //    MessageBox.Show("添加了平均噪声！");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("未添加平均噪声！");
+            //}
+        }
+
+        private void btn_Finish_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("您选择添加了" + strCollected, "提示");
+        }
     }
 }
